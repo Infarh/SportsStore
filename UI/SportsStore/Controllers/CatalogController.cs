76f4cfe7acb83be065902c725c40937d7e43e4f0
Infarh <shmachilin@yandex.ru>
2020-03-30 12:28:31 +0300

    Навигация

---
+++ UI/SportsStore/Controllers/CatalogController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
