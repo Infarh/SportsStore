6978260a38b2b7456a0773b1953b4ba4fc4b7f2c
Infarh <shmachilin@yandex.ru>
2020-03-30 11:47:35 +0300

    Хранилища объектов (в памяти)

---
+++ Services/SportsStore.Interfaces/Base/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Interfaces.Base
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> Items { get; }
+
+        void Add(T item);
+    }
+}
---
+++ Services/SportsStore.Services/Products/InMemory/ProductsRepositoryInMemory.cs
@@ -0,0 +1,9 @@
+﻿using SportsStore.Domain.Models;
+using SportsStore.Services.Base.InMemory;
+
+namespace SportsStore.Services.Products.InMemory
+{
+    public class ProductsRepositoryInMemory : RepositoryInMemory<Product>
+    {
+    }
+}
