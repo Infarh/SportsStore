ff931960b0c150b29479d7d47559e153ff277aed
Infarh <shmachilin@yandex.ru>
2020-03-31 16:37:11 +0300

    Добавление тестовых товаров в БД

--- Services/SportsStore.Services/Data/SportStoreDBInitializer.cs
+++ Services/SportsStore.Services/Data/SportStoreDBInitializer.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SportsStore.DAL.Context;
+using SportsStore.Domain.Models;
 
 namespace SportsStore.Services.Data
 {
@@ -15,6 +17,34 @@ namespace SportsStore.Services.Data
         public async Task InitializeAsync()
         {
             await _Context.Database.MigrateAsync().ConfigureAwait(false);
+
+            if (!await _Context.Products.AnyAsync().ConfigureAwait(false))
+            {
+                await _Context.Products.AddRangeAsync(
+                    new Product
+                    {
+                        Name = "Каяк", 
+                        Category = "Водный спорт",
+                        PurchasePrice = 200,
+                        RetailPrice = 275
+                    },
+                    new Product
+                    {
+                        Name = "Спасательный жилет",
+                        Category = "Водный спорт",
+                        PurchasePrice = 30,
+                        RetailPrice = 48.95m
+                    },
+                    new Product
+                    {
+                        Name = "Футбольный мяч",
+                        Category = "Футбол",
+                        PurchasePrice = 17,
+                        RetailPrice = 19.50m
+                    })
+                   .ConfigureAwait(false);
+                await _Context.SaveChangesAsync().ConfigureAwait(false);
+            }
         }
     }
 }
