39415c3b6bd9ffc80953bbb61bf912f6a6686def
Infarh <shmachilin@yandex.ru>
2020-03-31 15:58:26 +0300

    Базовый репозиторий сущностей в БД

---
+++ Services/SportsStore.Services/Base/InSQL/RepositoryInSQL.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.DAL.Context;
+using SportsStore.Domain.Models.Base.Interfaces;
+using SportsStore.Interfaces.Base;
+
+namespace SportsStore.Services.Base.InSQL
+{
+    public abstract class RepositoryInSQL<T> : IRepository<T> where T : class, IEntity
+    {
+        private readonly SportStoreDB _Context;
+
+        private DbSet<T> ItemsSet => _Context.Set<T>();
+
+        public virtual IQueryable<T> Query => ItemsSet;
+
+        public IEnumerable<T> Items => Query.AsEnumerable();
+
+        protected RepositoryInSQL(SportStoreDB Context) => _Context = Context;
+
+        public void Add(T item)
+        {
+            ItemsSet.Add(item);
+            _Context.SaveChanges();
+        }
+    }
+}
