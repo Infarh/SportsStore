42738124c54bac822564e3180d03ec7c5a12cf21
Infarh <shmachilin@yandex.ru>
2020-04-02 17:03:00 +0300

    Модель сущности Категория товаров

---
+++ Common/SportsStore.Domain/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿using SportsStore.Domain.Models.Base;
+
+namespace SportsStore.Domain.Models
+{
+    public class Category : NamesEntity
+    {
+        public string Description { get; set; }
+    }
+}
