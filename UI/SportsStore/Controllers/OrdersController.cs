17ccf80fd450384ddadb966a13696041e8519ae7
Infarh <shmachilin@yandex.ru>
2020-04-03 17:16:49 +0300

    Контроллер заказов

---
+++ UI/SportsStore/Controllers/OrdersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Interfaces.Products;
+
+namespace SportsStore.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IOrdersRepository _Orders;
+        private readonly IProductsRepository _Products;
+
+        public OrdersController(IOrdersRepository Orders, IProductsRepository Products)
+        {
+            _Orders = Orders;
+            _Products = Products;
+        }
+
+        public IActionResult Index() => View();
+    }
+}
