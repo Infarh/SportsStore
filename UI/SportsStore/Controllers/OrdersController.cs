f6c6b5d0b9f9a7d8758db55d504e90a08171c5f3
Infarh <shmachilin@yandex.ru>
2020-04-03 21:36:11 +0300

    Подготовка контроллера заказов

--- UI/SportsStore/Controllers/OrdersController.cs
+++ UI/SportsStore/Controllers/OrdersController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Domain.Models;
+using SportsStore.Domain.Models.Orders;
 using SportsStore.Interfaces.Products;
 
 namespace SportsStore.Controllers
@@ -14,6 +18,30 @@ namespace SportsStore.Controllers
             _Products = Products;
         }
 
-        public IActionResult Index() => View();
+        public IActionResult Index() => View(_Orders.Items);
+
+        public IActionResult Edit(long Id)
+        {
+            var order = Id == 0 ? new Order() : _Orders[Id];
+            if (order is null)
+                return NotFound();
+
+            var lines = order.Lines?.ToDictionary(line => line.ProductId) ?? new Dictionary<long, OrderLine>();
+            ViewBag.Lines = _Products.Items.Select(product => lines.TryGetValue(product.Id, out var line) 
+                ? line 
+                : new OrderLine
+                {
+                    Product = product, 
+                    ProductId = product.Id,
+                    Quantity = 0
+                });
+            return View(order);
+        }
+
+        public IActionResult Delete(Order Order)
+        {
+            _Orders.Delete(Order);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
