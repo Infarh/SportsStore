6eb4c7a50abf9b356a0a39f54ad939bb02f3a864
Infarh <shmachilin@yandex.ru>
2020-04-03 16:50:07 +0300

    Добавлена сущность Заказ

---
+++ Common/SportsStore.Domain/Models/Orders/OrderLine.cs
@@ -0,0 +1,13 @@
+﻿using SportsStore.Domain.Models.Base;
+
+namespace SportsStore.Domain.Models.Orders
+{
+    public class OrderLine : BaseEntity
+    {
+        public long ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public long OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
