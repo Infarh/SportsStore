7aa027e29a4e518bb87c154e91756c3cb9e87f54
Infarh <shmachilin@yandex.ru>
2020-04-12 13:32:36 +0300

    Обрабтка кодов ошибок

---
+++ UI/SportsStore/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index(string Code) => Code switch
+        {
+            "404" => View("Error404"),
+            _ => View(model: Code)
+        };
+    }
+}
--- UI/SportsStore/Startup.cs
+++ UI/SportsStore/Startup.cs
@@ -1,10 +1,13 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.WebEncoders.Testing;
 using SportsStore.DAL.Context;
 using SportsStore.Infrastructure.AutoMapper;
 using SportsStore.Services;
@@ -23,6 +26,7 @@ namespace SportsStore
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             services.AddSportStoreServices();
+            services.AddDirectoryBrowser();
 
             services.AddDbContext<SportStoreDB>(opt =>
             {
@@ -40,17 +44,27 @@ namespace SportsStore
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SportStoreDBInitializer db)
         {
             db.Initialize();
-            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
+                app.UseFileServer(new FileServerOptions
+                {
+                    EnableDefaultFiles = true,
+                    EnableDirectoryBrowsing = true,
+                    RequestPath = "/files"
+                });
+            }
+            else
+            {
+                app.UseDefaultFiles();
+                app.UseStaticFiles();
             }
 
             app.UseStatusCodePages();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
+            app.UseStatusCodePagesWithReExecute("/Error", "?code={0}");
+           
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
