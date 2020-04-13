a99da3ca84b4ca1d25ab1567325fa2235f29babf
Infarh <shmachilin@yandex.ru>
2020-04-13 13:59:43 +0300

    Восстановление работы статических файлов

--- UI/SportsStore/Startup.cs
+++ UI/SportsStore/Startup.cs
@@ -53,15 +53,12 @@ namespace SportsStore
                     RequestPath = "/files"
                 });
             }
-            else
-            {
-                app.UseDefaultFiles();
-                app.UseStaticFiles();
-            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseStatusCodePages();
             app.UseStatusCodePagesWithReExecute("/Error", "?code={0}");
-           
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
